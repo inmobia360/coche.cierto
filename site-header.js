ee1df2263c91fec556a7735100881c397bfe5716
cochecierto <inmobia360@gmail.com>
2026-09-03 18:32:25 +0200

    fix: reliably auto-hide beta notice

--- site-header.js
+++ site-header.js
@@ -185,8 +185,12 @@
         beta.setAttribute('role', 'status');
         beta.innerHTML = '<strong>Versión beta</strong><span>Estamos probando CocheCierto. Los resultados son orientativos y pueden cambiar mientras validamos la plataforma.</span>';
         (document.querySelector('.site-shell') || document.body).prepend(beta);
+      }
+      var betaNotice = document.querySelector('.beta-notice');
+      if (betaNotice && !betaNotice.dataset.autoHideScheduled) {
+        betaNotice.dataset.autoHideScheduled = 'true';
         window.setTimeout(function () {
-          if (beta.isConnected) beta.remove();
+          if (betaNotice.isConnected) betaNotice.remove();
         }, 10000);
       }
     }
